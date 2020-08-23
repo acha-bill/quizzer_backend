e40cf2b10eafb0608100690dee3b97902cf8e8e9
Daniel Carlson <dancarl758@gmail.com>
2020-08-23 02:30:48 +0100

    feat(api/v1): Added mongodb configuration

--- main.go
+++ main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
+	"context"
+	"net/http"
+	"os"
+
 	"github.com/joho/godotenv"
 	"github.com/labstack/gommon/log"
-	"net/http"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func main() {
+var collection *mongo.Collection
+var ctx = context.TODO()
+
+func init() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(err.Error())
 		return
 	}
 
+	mongoURL := os.Getenv("MONGODB_URL")
+	dbName := os.Getenv("DATABASE_NAME")
+
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database(dbName).Collection("testCollection")
+}
+
+func main() {
 	// Echo instance
 	e := echo.New()
 
